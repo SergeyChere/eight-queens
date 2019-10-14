50287ea7fc19488bfa0b2ac8f763c663f894f1de
Cherepanov Sergey <che.sergii@gmail.com>
2019-10-15 01:17:46 +0300

    Added rolling method

---
+++ src/FindingAvailableAnswersByRollingBoard.java
@@ -0,0 +1,23 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public interface FindingAvailableAnswersByRollingBoard {
+
+    static void rollingBoard(ArrayList<ArrayList<Integer>> list, int length) {
+        for (int sides = 0; sides<3; sides++) {
+            ArrayList<Integer> tempArray = new ArrayList<>();
+            for (int indexElement = 0; indexElement<length; indexElement++) {
+                tempArray.add(list.get(indexElement).get(1));
+            }
+            for (int i=1; i<=length; i++) {
+                Integer index = tempArray.indexOf(i);
+                Integer element = length-index;
+                list.get(i-1).remove(1);
+                list.get(i-1).add(element);
+            }
+            if (CheckingAvailability.checkingAvailabilityOfDoubleArrayList(list)) {
+                System.out.println("    Rolled: "+ Arrays.deepToString(list.toArray()));
+            }
+        }
+    }
+}
--- src/Test.java
+++ src/Test.java
@@ -3,26 +3,27 @@ import java.util.*;
 public class Test {
 
     public static final int length = 8;
-//    public static ArrayList<ArrayList<Integer>> list = new ArrayList<>();
+    public static ArrayList<ArrayList<Integer>> list = new ArrayList<>();
 
     public static void main(String[] args) {
-//        list = FindingFirstAvailableAnswer.findingFirstElement(length);
+        list = FindingFirstAvailableAnswer.findingFirstElement(length);
+        FindingAvailableAnswersByRollingBoard.rollingBoard(list, length);
 
         //ADDING
-        int counter = 1;
-        for (ArrayList<ArrayList<Integer>> list: AllAnswersHardlyAdded.fillingArray()) {
-            System.out.println("********************** "+counter+" ************************");
-//            if (!CheckingAvailability.checkingAvailabilityOfDoubleArrayList(list)) {
-//                System.out.println("WRONG ARRAY: "+Arrays.deepToString(list.toArray()));
-//            }
-            System.out.println("to check: "+Arrays.deepToString(list.toArray()));
-            System.out.println("------------------- ADDING ---------------------------");
-            FindingAvailableAnswersByAddingToEveryElement.addingElements(list, length);
-            System.out.println("------------------- MOVING ---------------------------");
-            FindingAvailableAnswersByMovingSecondElementOfArray.movingElement(list, length);
-            System.out.println();
-            counter++;
-        }
+//        int counter = 1;
+//        for (ArrayList<ArrayList<Integer>> list: AllAnswersHardlyAdded.fillingArray()) {
+//            System.out.println("********************** "+counter+" ************************");
+////            if (!CheckingAvailability.checkingAvailabilityOfDoubleArrayList(list)) {
+////                System.out.println("WRONG ARRAY: "+Arrays.deepToString(list.toArray()));
+////            }
+//            System.out.println("to check: "+Arrays.deepToString(list.toArray()));
+//            System.out.println("------------------- ADDING ---------------------------");
+//            FindingAvailableAnswersByAddingToEveryElement.addingElements(list, length);
+//            System.out.println("------------------- MOVING ---------------------------");
+//            FindingAvailableAnswersByMovingSecondElementOfArray.movingElement(list, length);
+//            System.out.println();
+//            counter++;
+//        }
 
         //SWAPPING
 //        System.out.println("------------------- SWAPPING ---------------------------");
